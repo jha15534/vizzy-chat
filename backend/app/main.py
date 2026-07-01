06e940d901f74484fd733eb45f9b51caf8cfe172
Shruti Jha <133661363+jha15534@users.noreply.github.com>
2026-07-01 13:17:27 +0530

    Fix CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,31 +4,31 @@ from app.models.chat import Chat
 from app.models.message import Message
 from app.routes.history import router as history_router
 from app.routes.chat import router as chat_router
-
 from app.routes.image import router as image_router
-
 from app.database import Base, engine
-
 from app.models.user import User
-
 from app.routes.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI(title="Vizzy Chat API")
 
+# CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://vizzy-chat-flame.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Routers
 app.include_router(auth_router)
-
 app.include_router(chat_router)
-
 app.include_router(image_router)
-
 app.include_router(history_router)
 
 @app.get("/")
